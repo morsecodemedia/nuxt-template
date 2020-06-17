60083f3132138757501ffd38d033d7de9bdf4a77
Brandon Morse <brandon@morsecodemedia.com>
2020-06-17 02:02:57 -0400

    Initial commit of Nuxt Template. Adds a few modules and stubbed config settings. Removes unnecessary things that come with a default Nuxt site.

---
+++ README.md
@@ -0,0 +1,35 @@
+# nuxt-template
+
+> A template to start Nuxt projects, loaded with all the additional modules that are used in almost all of our projects.
+
+## Logs
+`$ yarn add @nuxtjs/sentry`
+`$ yarn add nuxt-logrocket`
+
+## Front-End Frameworks
+`$ yarn add -D @morsecodemedia/containers.css`
+
+## Fonts
+`$ yarn add nuxt-webfontloader`
+
+## Input Mask
+`$ yarn add vue-the-mask`
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>
+    Hello World!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  head () {
+    return {
+      title: '',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: ''
+        },
+        { hid: 'ogtitle', property: 'og:title', content: '' },
+        { hid: 'ogurl', property: 'og:url', content: 'https://www.domain.com/' },
+        { hid: 'ogdescription', property: 'og:description', content: '' },
+        { hid: 'twsite', name: 'twitter:site', content: 'https://www.domain.com/' },
+        { hid: 'twtitle', name: 'twitter:title', content: '' },
+        { hid: 'twdescription', name: 'twitter:description', content: '' },
+        { hid: 'googlename', itemprop: 'name', content: '' },
+        { hid: 'googledescription', itemprop: 'description', content: '' }
+      ],
+      link: [
+        { hid: 'canonical', rel: 'canonical', href: 'https://www.domain.com/' + this.$route.path }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
